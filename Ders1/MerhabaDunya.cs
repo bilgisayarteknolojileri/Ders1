c697a7bb01ecc62113c5bb01c9e3440ccbca688f
bl2-18 <bl2-18@DESKTOP-NQAFT0U>
2022-02-16 09:46:03 +0300

    Proje dosyası ekle.

---
+++ Ders1/MerhabaDunya.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders1
+{
+    class Ders1
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("AMMAR ASLAN"+" BATMAN");
+        }
+    }
+}
